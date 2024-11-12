c702a310f6f246243687c72f50e635ec7e3c4c0b
Sachinkisna <sachinsoftware0001@gmail.com>
2024-11-12 11:13:06 +0530

    initial comments

---
+++ src/Java_Variables.java
@@ -0,0 +1,8 @@
+public class Java_Variables {
+    public static void main(String[] args) {
+        System.out.println("Java Variables-");
+        int intVariable=52;
+        System.out.println(intVariable);
+        System.out.println("Variable names are like identifiers naming conventions- camelCase");
+    }
+}
